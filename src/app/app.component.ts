ea1219205c5202e3424657e0f51d1198bbe9cd5d
pdawson2090 <36117746+pdawson2090@users.noreply.github.com>
2018-03-14 18:02:40 -0400

    pipeline changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AccordionModule } from 'primeng/accordion';     //accordion and accordion tab
+import { MenuItem } from 'primeng/api';                 //api
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
