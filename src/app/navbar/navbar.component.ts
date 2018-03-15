b8ffdd0a7d5dad80cd3e05ef6506bf89b2137970
pdawson2090 <36117746+pdawson2090@users.noreply.github.com>
2018-03-15 15:50:16 -0400

    new dev update

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  //encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./navbar.component.css']
+  
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
