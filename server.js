001e3355cd5e1170e4a7a26022eaf80d57bd592b
pdawson2090 <pedawso1@millersville.edu>
2018-03-22 21:58:12 -0400

    x

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.use(function(req, res) {
-    res.sendfile(__dirname + '/dist/index.html');
-});
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
